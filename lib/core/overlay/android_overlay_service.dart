1371c12768e8aea9a1b4dbd7205909425ef49042
Sebas0914 <147787208+Sebas0914@users.noreply.github.com>
2026-09-21 11:36:00 -0700

    feat: add Flutter Android overlay bridge

---
+++ lib/core/overlay/android_overlay_service.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/services.dart';
+
+import 'overlay_service.dart';
+
+class AndroidOverlayService implements OverlayService {
+  const AndroidOverlayService();
+
+  static const _channel = MethodChannel('great_sage_mobile/overlay');
+
+  @override
+  Future<bool> isSupported() async {
+    try {
+      return await _channel.invokeMethod<bool>('isSupported') ?? false;
+    } on PlatformException {
+      return false;
+    }
+  }
+
+  Future<bool> hasPermission() async {
+    try {
+      return await _channel.invokeMethod<bool>('hasPermission') ?? false;
+    } on PlatformException {
+      return false;
+    }
+  }
+
+  @override
+  Future<bool> requestPermission() async {
+    try {
+      return await _channel.invokeMethod<bool>('requestPermission') ?? false;
+    } on PlatformException {
+      return false;
+    }
+  }
+
+  @override
+  Future<void> showRaphael() async {
+    await _channel.invokeMethod<void>('show');
+  }
+
+  @override
+  Future<void> hideRaphael() async {
+    await _channel.invokeMethod<void>('hide');
+  }
+}
